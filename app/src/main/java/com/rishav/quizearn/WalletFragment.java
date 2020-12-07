2bc1ebaa3141baa603a4f91d1a7e660348c55580
Rishav Chanda <rishavchanda0@gmail.com>
2020-12-07 20:33:07 +0530

    quiz earn

--- app/src/main/java/com/rishav/quizearn/WalletFragment.java
+++ app/src/main/java/com/rishav/quizearn/WalletFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -31,6 +32,7 @@ public class WalletFragment extends Fragment {
     }
     FragmentWalletBinding binding;
     FirebaseFirestore database;
+    Users user;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,12 +45,36 @@ public class WalletFragment extends Fragment {
                 .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                Users user = documentSnapshot.toObject(Users.class);
+                user = documentSnapshot.toObject(Users.class);
                 binding.currentCoins.setText(String.valueOf(user.getCoins()));
             }
         });
 
+        binding.sendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.getCoins() > 50000){
+                    String uid = FirebaseAuth.getInstance().getUid();
+                    String paypal = binding.paypalEmailid.getText().toString();
+                    if(binding.paypalEmailid == null) {
+                        Toast.makeText(getContext(),"Please enter Paypal Emailid",Toast.LENGTH_SHORT).show();
+                    }else {
+                        withdrawRequest request = new withdrawRequest(uid, paypal,user.getName());
+                        database.collection("withdraws")
+                                .document(uid)
+                                .set(request).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getContext(),"Request sent Sucessfully !..Your Money will be transfered within 4 to 5 days.",Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
+                    }
+                }else {
+                    Toast.makeText(getContext(),"You need more coins to withdraw money.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         return binding.getRoot();
