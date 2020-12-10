e1cb8573768b4aeb96cf02129de5d639f09df36d
Rishav Chanda <rishavchanda0@gmail.com>
2020-12-10 12:17:52 +0530

    quiz earn

--- app/src/main/java/com/rishav/quizearn/MyFirebaseMessagingService.java
+++ app/src/main/java/com/rishav/quizearn/MyFirebaseMessagingService.java
@@ -7,24 +7,10 @@ import androidx.core.app.NotificationManagerCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-class MyFirebaseMessagingService extends FirebaseMessagingService
-{
-
-
- @Override
- public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-  super.onMessageReceived(remoteMessage);
-  getFirebaseMessage(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
- }
-
- public void getFirebaseMessage(String title, String msg){
-  NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"myQuizEarnChannel")
-          .setContentTitle(title)
-          .setContentText(msg)
-          .setAutoCancel(true);
-
-  NotificationManagerCompat manager = NotificationManagerCompat.from(this);
-  manager.notify(101,builder.build());
- }
+class MyFirebaseMessagingService extends FirebaseMessagingService {
+   @Override
+   public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+    super.onMessageReceived(remoteMessage);
+   }
 }
 
