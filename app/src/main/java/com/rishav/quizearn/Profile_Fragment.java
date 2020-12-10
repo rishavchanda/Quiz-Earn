9e5da0f1253dd0bf2366de189c1c7756c4625884
Rishav Chanda <rishavchanda0@gmail.com>
2020-12-10 11:04:58 +0530

    quiz earn

--- app/src/main/java/com/rishav/quizearn/Profile_Fragment.java
+++ app/src/main/java/com/rishav/quizearn/Profile_Fragment.java
@@ -250,7 +250,7 @@ public class Profile_Fragment extends Fragment {
                logoutDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                    @Override
                    public void onClick(DialogInterface dialog, int which) {
-
+                       pd.dismiss();
                    }
                });
 
