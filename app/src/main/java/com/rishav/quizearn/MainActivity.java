b924cb0dfdcc6efda1ce2019f6f7846d37568f97
Rishav Chanda <rishavchanda0@gmail.com>
2020-12-27 21:55:53 +0530

    quizearn

--- app/src/main/java/com/rishav/quizearn/MainActivity.java
+++ app/src/main/java/com/rishav/quizearn/MainActivity.java
@@ -2,8 +2,13 @@ package com.rishav.quizearn;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -14,6 +19,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePageAdapter pageAdapter;
+    SharedPreferences mSharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +31,51 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, Login.class));
-                finish();
+                mSharedPref = getSharedPreferences("SharedPref",MODE_PRIVATE);
+                boolean isFirstTime = mSharedPref.getBoolean("firstTime",true);
+                if(isFirstTime){
+                    //onboarding
+                    SharedPreferences.Editor editor = mSharedPref.edit();
+                    editor.putBoolean("firstTime",false);
+                    editor.commit();
+                }else {
+                    startActivity(new Intent(MainActivity.this, Login.class));
+                    finish();
+                }
             }
         },1500);
+
+        viewPager=findViewById(R.id.pager);
+        pageAdapter = new ScreenSlidePageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+    }
+
+    private class ScreenSlidePageAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePageAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                return tab1;
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
