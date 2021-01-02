04abc96b14d5278073601b2eba981b5c1177b218
Rishav Chanda <rishavchanda0@gmail.com>
2021-01-02 16:08:50 +0530

    quizearn

--- app/src/main/java/com/rishav/quizearn/Dashboard.java
+++ app/src/main/java/com/rishav/quizearn/Dashboard.java
@@ -1,49 +1,25 @@
 package com.rishav.quizearn;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.rishav.quizearn.databinding.ActivityDashboardBinding;
-import com.rishav.quizearn.databinding.ActivityMainBinding;
-import com.rishav.quizearn.databinding.FragmentHomeBinding;
-
-import java.util.ArrayList;
 
 import me.ibrahimsn.lib.OnItemSelectedListener;
 
-public class Dashboard extends AppCompatActivity {
-    ImageView logout;
+public class Dashboard extends AppCompatActivity  {
     ActivityDashboardBinding binding;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
